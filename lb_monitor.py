b46680d0d0c8bf9fe635f465b3e29ace36a788d6
zui0711 <626255007@qq.com>
2025-06-30 19:45:16 +0800

    Update lb_monitor.py

--- lb_monitor.py
+++ lb_monitor.py
@@ -10,12 +10,14 @@ import warnings
 warnings.filterwarnings('ignore')
 
 if __name__ == '__main__':
+    # 参数1 比赛名
+    # 参数2 最近提交时间
     competition_name = sys.argv[1]
     sub_time = float(sys.argv[2])
     api = KaggleApi()
     api.authenticate()
     # 获取排行榜
-
+    last_team = None
     while(True):
         now = datetime.now()
         print(now.strftime("%Y-%m-%d %H:%M:%S"))  # 2023-06-30 14:25:36
@@ -23,7 +25,7 @@ if __name__ == '__main__':
         leaderboard = api.competition_leaderboard_view(competition_name)
 
         leaderboard_data = []
-        for i, entry in enumerate(leaderboard[:30]):
+        for i, entry in enumerate(leaderboard[:50]):
             leaderboard_data.append({
                 'Rank': i + 1,
                 'teamName': entry.team_name,
@@ -35,6 +37,13 @@ if __name__ == '__main__':
 
         ld_df['submissionDate'] = ld_df['submissionDate'].dt.tz_localize('UTC').dt.tz_convert('Asia/Shanghai')
         ld_df['time_diff'] = ((pd.Timestamp.now(tz='Asia/Shanghai')-ld_df['submissionDate']).dt.total_seconds() / 60).round(2)
+        print('最近提交...')
         print(ld_df[ld_df['time_diff']<sub_time])
+        print('------')
+        if last_team is not None:
+            print('新进入排行榜...')
+            print(ld_df[~ld_df['teamName'].isin(last_team)])
+        last_team = ld_df['teamName'].unique()
+
         print('===============================\n')
         time.sleep(300)
